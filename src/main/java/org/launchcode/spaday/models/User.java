b46d0fc84b091b320420cc38e0cbea5b5362cd2c
Heather Hecky <hhecky@gmail.com>
2020-10-05 20:29:25 -0500

    finished studio w/Doug in class

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,8 +1,23 @@
 package org.launchcode.spaday.models;
 
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+
 public class User {
+
+    @NotBlank(message = "User name is required.")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters long.")
     private String username;
+
+    @Email(message = "Email is optional but if you provide one it must be a valid email address.")
     private String email;
+
+    @NotBlank(message = "Password is required.")
+    @Size(min =6, message = "Sorry, but the given password is too short. Passwords must be at least 6 characters long.")
     private String password;
 
     public User() {
